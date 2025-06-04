eabc77c69f27a61139b3aa001be0e8f12ad747a2
LogPiyo <shushu20011031@gmail.com>
2025-06-05 02:23:28 +0900

    feat: サイトタイトル変更

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,5 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'WeeThink';
+export const SITE_TITLE_FOR_SEO = 'WeeThink: 物理・数学・プログラミングのブログ';
 export const SITE_DESCRIPTION = 'Welcome to my website!';
