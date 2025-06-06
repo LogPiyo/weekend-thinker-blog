56766500cf6cd780acc5484f69635a94d585a1cf
LogPiyo <shushu20011031@gmail.com>
2025-06-06 16:24:03 +0900

    fix: サイト名変更

--- src/consts.ts
+++ src/consts.ts
@@ -1,6 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'WeeThink';
-export const SITE_TITLE_FOR_SEO = 'WeeThink: 物理・数学・プログラミングのブログ';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Wekilo';
+export const SITE_TITLE_FOR_SEO = 'Wekilo: ウェキロー 物理・数学・プログラミングのブログ';
+export const SITE_DESCRIPTION = 'Welcome to my Wekilo!';
