e930eccf183f74527ea88ae407f4423c69499ad6
LogPiyo <shushu20011031@gmail.com>
2025-06-06 16:38:27 +0900

    fix: お問い合わせURLを変更

--- src/consts.ts
+++ src/consts.ts
@@ -4,3 +4,4 @@
 export const SITE_TITLE = 'Wekilo';
 export const SITE_TITLE_FOR_SEO = 'Wekilo: ウェキロー 物理・数学・プログラミングのブログ';
 export const SITE_DESCRIPTION = 'Welcome to my Wekilo!';
+export const CONTACT_US_URL = "https://docs.google.com/forms/d/e/1FAIpQLSdR3UlcJEcHyKptJmpnfOOiunOQ7XwJzr-qPdjRhXFOxh0EdQ/viewform?usp=pp_url"
