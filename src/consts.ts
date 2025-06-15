73ee9bee7ab2e4fd0a6b29ea4b07227b0422b120
LogPiyo <shushu20011031@gmail.com>
2025-06-16 00:39:15 +0900

    fix: 不要コード削除・改行の調整

--- src/consts.ts
+++ src/consts.ts
@@ -1,6 +1,3 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
 export const SITE_TITLE = 'Wekilo';
 export const SITE_TITLE_FOR_SEO = 'Wekilo: ウェキロー 物理・数学・プログラミングのブログ';
 export const SITE_DESCRIPTION = 'Welcome to my Wekilo!';
