0fbcfaea5b65dead7ec2e9bd35484e93a77dd410
LogPiyo <shushu20011031@gmail.com>
2025-07-21 00:29:38 +0900

    fix: KaTeXのバージョンを0.16.4から0.16.22に更新

--- astro.config.mjs
+++ astro.config.mjs
@@ -58,8 +58,8 @@ export default defineConfig({
           tag: 'link',
           attrs: {
             rel: 'stylesheet',
-            href: 'https://cdn.jsdelivr.net/npm/katex@0.16.4/dist/katex.min.css',
-            integrity: 'sha384-vZTGshhcJwVjMEcoCsoDhbLMegZbDrg6eRmFwhl/8tSru3CTb5EgGF9aFLPeANtO',
+            href: 'https://cdn.jsdelivr.net/npm/katex@0.16.22/dist/katex.min.css',
+            integrity: 'sha384-5TcZemv2l/9On385z///+d7MSYlvIEw9FuZTIdZ14vJLqWphw7e7ZPuOiCHJcFCP',
             crossorigin: 'anonymous',
           }
         }
